3fa040ad676aa7ffe39746ffdf2e999d05698842
6Morpheus6 <wanton-decay@gmx.net>
2025-05-21 02:40:57 +0200

    unpin gradio

--- install.js
+++ install.js
@@ -28,7 +28,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install gradio==4.43.0 pydantic==2.10.6 numpy==1.26.3 devicetorch",
+          "uv pip install gradio numpy==1.26.3 devicetorch",
           "uv pip install -r requirements.txt"
         ]
       }
